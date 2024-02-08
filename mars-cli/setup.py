beb7124d3bc97fe192734e47f4cb694ff9c51ce8
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-02-08 11:14:21 +0100

    Add readme file as long description

--- mars-cli/setup.py
+++ mars-cli/setup.py
@@ -1,12 +1,17 @@
+import pathlib
 from _version import __version__
 from setuptools import find_packages, setup
 
 with open("requirements.txt", "r") as file:
     required_deps = file.read().splitlines()
 
+parent_folder = pathlib.Path(__file__).parent.resolve()
+long_description = (parent_folder / "README.md").read_text(encoding="utf-8")
+
 setup(
     name="mars",
     description="Multi-omics Adapter for Repository Submissions",
+    long_description=long_description,
     packages=find_packages(include=["mars-lib"]),
     version=__version__,
     license="MIT",
