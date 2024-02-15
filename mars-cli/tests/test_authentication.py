6536dc82decd5045b0ed6a8c0b5e36f3cbc1e88f
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-02-15 10:16:38 +0100

    Add extra test

--- mars-cli/tests/test_authentication.py
+++ mars-cli/tests/test_authentication.py
@@ -1,9 +1,11 @@
 import pytest
+import json
+import os
 from mars_lib.authentication import get_webin_auth_token
 
 
 def test_get_webin_auth_token():
-    credentials_dict = {
+    fake_credentials_dict = {
         "username": "my_fake_account",
         "password": "my_super_secret_password",
     }
@@ -11,4 +13,12 @@ def test_get_webin_auth_token():
         ValueError,
         match="ERROR when generating token. See response's content below:\nBad credentials",
     ):
-        get_webin_auth_token(credentials_dict)
+        get_webin_auth_token(fake_credentials_dict)
+
+    file = "./tests/test_credentials.json"
+    if os.path.exists(file) and os.path.isfile(file):
+        with open(file, "r") as f:
+            test_credentials = json.load(f)
+
+        token = get_webin_auth_token(test_credentials)
+        assert token
