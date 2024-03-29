0c536e38dac01c5762680bc1ac8949f866a83785
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-03-29 13:03:41 +0100

    Add logging

--- mars-cli/mars_cli.py
+++ mars-cli/mars_cli.py
@@ -1,56 +1,31 @@
-from mars_lib.authentication import get_webin_auth_token
-from mars_lib.biosamples_external_references import (
-    get_header,
-    biosamples_endpoints,
-    BiosamplesRecord,
-    validate_json_against_schema,
-    handle_input_dict,
-    input_json_schema_filepath,
-)
+from math import log
 import click
 import logging
+import pathlib
+from configparser import ConfigParser
 from mars_lib.isa_json import TargetRepository
 
+# Load CLI configuration
+home_dir = pathlib.Path.home()
+config_file = home_dir / ".mars" / "settings.ini"
+fallback_log_file = home_dir / ".mars" / "app.log"
 
-def create_external_references(
-    biosamples_credentials, biosamples_externalReferences, production
-):
-    """
-    Main function to be executed when script is run.
+config = ConfigParser()
+config.read(config_file)
 
-    Args:
-    biosamples_credentials: Dictionary with the credentials of the submitter of the existing Biosamples records.
-    biosamples_externalReferences: Dictionary containing the mapping between the
-    production: Boolean indicating the environment of BioSamples to use.
-    """
-    if production:
-        logger_name = "production"
-        biosamples_endpoint = biosamples_endpoints["prod"]
-    else:
-        logger_name = "development"
-        biosamples_endpoint = biosamples_endpoints["dev"]
+# Logging configuration
+log_level = config.get("logging", "log_level", fallback="ERROR")
+log_file = config.get("logging", "log_file", fallback=fallback_log_file)
+logging.basicConfig(
+    filename=log_file,
+    level=log_level,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
-        logging.basicConfig(
-            filename=logger_name + ".log",
-            filemode="w",
-            format="%(name)s - %(levelname)s - %(message)s",
-        )
 
-    validate_json_against_schema(
-        json_doc=biosamples_externalReferences, json_schema=input_json_schema_filepath
-    )
-    token = get_webin_auth_token(biosamples_credentials)
-    header = get_header(token)
-
-    for biosample_r in biosamples_externalReferences["biosampleExternalReferences"]:
-        bs_accession = biosample_r["biosampleAccession"]
-        BSrecord = BiosamplesRecord(bs_accession)
-        BSrecord.fetch_bs_json(biosamples_endpoint)
-        # To test it without the fetching, you can download it manually and then use:
-        #   BSrecord.load_bs_json(bs_json_file="downloaded-json.json")
-        new_ext_refs_list = biosample_r["externalReferences"]
-        BSrecord.extend_externalReferences(new_ext_refs_list)
-        BSrecord.update_remote_record(header)
+def print_and_log(msg):
+    click.echo(msg)
+    logging.info(msg)
 
 
 @click.group()
@@ -60,7 +35,7 @@ def create_external_references(
     help="Boolean indicating the usage of the development environment of the target repositories. If not present, the production instances will be used.",
 )
 def cli(development):
-    click.echo(
+    print_and_log(
         f"Running in {'Development environment' if development else 'Production environment'}"
     )
 
@@ -82,6 +57,7 @@ def cli(development):
     help="Submit to Metabolights.",
 )
 def submit(credentials_file, isa_json_file, submit_to_ena, submit_to_metabolights):
+    """Start a submission to the target repositories."""
     target_repositories = ["biosamples"]
     if submit_to_ena:
         target_repositories.append(TargetRepository.ENA)
@@ -89,14 +65,15 @@ def submit(credentials_file, isa_json_file, submit_to_ena, submit_to_metabolight
     if submit_to_metabolights:
         target_repositories.append(TargetRepository.METABOLIGHTS)
 
-    click.echo(
+    print_and_log(
         f"Staring submission of the ISA JSON to the target repositories: {', '.join(target_repositories)}."
     )
 
 
 @cli.command()
 def health_check():
-    click.echo("Checking the health of the target repositories.")
+    """Check the health of the target repositories."""
+    print_and_log("Checking the health of the target repositories.")
 
 
 if __name__ == "__main__":
