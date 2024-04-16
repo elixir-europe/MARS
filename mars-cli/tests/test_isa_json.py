96826b99cd9b601a767d7e3225e3020cd4f8ea19
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-04-16 08:56:52 +0200

    add validator tests

--- mars-cli/tests/test_isa_json.py
+++ mars-cli/tests/test_isa_json.py
@@ -1,10 +1,11 @@
 from mars_lib.isa_json import (
     reduce_isa_json_for_target_repo,
     load_isa_json,
-    TargetRepository,
 )
+from mars_lib.target_repo import TargetRepository, TARGET_REPO_KEY
 import pytest
 from pydantic import ValidationError
+from mars_lib.schemas.model import Data, Material, Assay, Person
 
 
 def test_load_isa_json():
@@ -30,3 +31,109 @@ def test_reduce_isa_json_for_target_repo():
 
     assert len(good_isa_json_study.assays) == 5
     assert len(filtered_isa_json_study.assays) == 1
+
+
+def test_data_type_validator():
+    valid_data_json = {"@id": "data_001", "name": "data 1", "type": "Image File"}
+
+    invalid_data_json = {
+        "@id": "data_001",
+        "name": "data 1",
+        "type": "Custom File",  # This is not a valid data type
+    }
+
+    assert Data.model_validate(valid_data_json)
+
+    with pytest.raises(ValidationError):
+        Data.model_validate(invalid_data_json)
+
+
+def test_material_type_validator():
+    valid_material_json = {
+        "@id": "material_001",
+        "name": "material 1",
+        "type": "Extract Name",
+    }
+
+    invalid_material_json = {
+        "@id": "material_002",
+        "name": "material 2",
+        "type": "Custom Material",  # This is not a valid material type
+    }
+
+    assert Material.model_validate(valid_material_json)
+
+    with pytest.raises(ValidationError):
+        Material.model_validate(invalid_material_json)
+
+
+def test_target_repo_comment_validator():
+    valid_assay_json = {
+        "@id": "assay_001",
+        "comments": [
+            {
+                "@id": "comment_001",
+                "name": "target repository",
+                "value": TargetRepository.ENA,
+            }
+        ],
+    }
+
+    invalid_assay_json = {
+        "@id": "assay_002",
+        "comments": [
+            {
+                "@id": "comment_002",
+                "name": "target repository",
+                "value": "my special repo",
+            }
+        ],
+    }
+
+    second_invalid_assay_json = {"@id": "assay_003", "comments": []}
+
+    third_invalid_assay_json = {
+        "@id": "assay_004",
+        "comments": [
+            {
+                "@id": "comment_003",
+                "name": "target repository",
+                "value": TargetRepository.ENA,
+            },
+            {
+                "@id": "comment_004",
+                "name": "target repository",
+                "value": TargetRepository.METABOLIGHTS,
+            },
+        ],
+    }
+
+    assert Assay.model_validate(valid_assay_json)
+    with pytest.raises(
+        ValidationError, match="Invalid 'target repository' value: 'my special repo'"
+    ):
+        Assay.model_validate(invalid_assay_json)
+
+    with pytest.raises(ValidationError, match="'target repository' comment is missing"):
+        Assay.model_validate(second_invalid_assay_json)
+
+    with pytest.raises(
+        ValidationError, match="Multiple 'target repository' comments found"
+    ):
+        Assay.model_validate(third_invalid_assay_json)
+
+    def test_person_phone_nr_validator():
+        valid_person_json = {
+            "@id": "person_001",
+            "phone_nr": "+49123456789",
+        }
+
+        invalid_person_json = {
+            "@id": "person_002",
+            "phone_nr": "123456789",
+        }
+
+        assert Person.model_validate(valid_person_json)
+
+        with pytest.raises(ValidationError, match="Invalid number format"):
+            Person.model_validate(invalid_person_json)
