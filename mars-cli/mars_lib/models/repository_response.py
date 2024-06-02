d6f11f4ea989472a30964ea1d1f7dadbf366297a
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-06-02 09:00:51 +0200

    Add class methods to create a RepositoryResponse from JSON

--- mars-cli/mars_lib/models/repository_response.py
+++ mars-cli/mars_lib/models/repository_response.py
@@ -1,8 +1,9 @@
-from ast import List
-from email import message
-from os import name
-from pydantic import BaseModel, field_validator, Field
-from target_repo import TargetRepository
+import json
+from typing import List, Optional
+from pydantic import BaseModel, field_validator, Field, ConfigDict
+import pydantic
+import pydantic.alias_generators
+from mars_lib.target_repo import TargetRepository
 
 
 class Filter(BaseModel):
@@ -12,7 +13,7 @@ class Filter(BaseModel):
 
 class Path(BaseModel):
     key: str
-    where: List[Filter] = Field(default_value=[])
+    where: Optional[Filter] = Field(default_value=None)
 
 
 class Accession(BaseModel):
@@ -31,8 +32,11 @@ class Info(BaseModel):
     message: str
 
 
-class Reponse(BaseModel):
-    target_repository: str = Field(alias="targetRepository")
+class RepositoryResponse(BaseModel):
+    # This is a Pydantic configuration that will convert the field names to camel case and be accessible as alias.
+    model_config = ConfigDict(alias_generator=pydantic.alias_generators.to_camel)
+
+    target_repository: str
     accessions: List[Accession] = Field(default_value=[])
     errors: List[Error] = Field(default_value=[])
     info: List[Info] = Field(default_value=[])
@@ -42,3 +46,36 @@ class Reponse(BaseModel):
         if v not in [item.value for item in TargetRepository]:
             raise ValueError(f"Invalid 'target repository' value: '{v}'")
         return v
+
+    @classmethod
+    def from_json_file(cls, json_file):
+        with open(json_file, "r") as file:
+            data = json.load(file)
+
+        target_repository = data.get("targetRepository")
+        accessions = [Accession(**acc) for acc in data.get("accessions", [])]
+        errors = [Error(**err) for err in data.get("errors", [])]
+        info = [Info(**inf) for inf in data.get("info", [])]
+
+        return cls(
+            targetRepository=target_repository,
+            accessions=accessions,
+            errors=errors,
+            info=info,
+        )
+
+    @classmethod
+    def from_json(cls, json_string: str):
+        data = json.loads(json_string)
+
+        target_repository = data.get("targetRepository")
+        accessions = [Accession(**acc) for acc in data.get("accessions", [])]
+        errors = [Error(**err) for err in data.get("errors", [])]
+        info = [Info(**inf) for inf in data.get("info", [])]
+
+        return cls(
+            targetRepository=target_repository,
+            accessions=accessions,
+            errors=errors,
+            info=info,
+        )
