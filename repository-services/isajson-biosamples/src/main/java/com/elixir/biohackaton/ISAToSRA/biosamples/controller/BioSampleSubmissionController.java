b26723e253f8afe08291c6389f14eef1128ced9e
dipayan1985 <dgupta@ebi.ac.uk>
2024-08-19 11:32:40 +0100

    BioSamples response as per receipt format

--- repository-services/isajson-biosamples/src/main/java/com/elixir/biohackaton/ISAToSRA/biosamples/controller/BioSampleSubmissionController.java
+++ repository-services/isajson-biosamples/src/main/java/com/elixir/biohackaton/ISAToSRA/biosamples/controller/BioSampleSubmissionController.java
@@ -4,6 +4,9 @@ package com.elixir.biohackaton.ISAToSRA.biosamples.controller;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
+import com.elixir.biohackaton.ISAToSRA.biosamples.receipt.Receipt;
+import com.elixir.biohackaton.ISAToSRA.biosamples.receipt.Receipt.Accession;
+import com.elixir.biohackaton.ISAToSRA.biosamples.receipt.Receipt;
 import com.elixir.biohackaton.ISAToSRA.biosamples.service.BioSamplesSubmitter;
 import com.elixir.biohackaton.ISAToSRA.model.IsaJson;
 import com.elixir.biohackaton.ISAToSRA.model.Study;
@@ -12,6 +15,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,22 +29,22 @@ public class BioSampleSubmissionController {
   @Autowired private ObjectMapper objectMapper;
 
   @ApiResponses(
-      value = {
-        @ApiResponse(responseCode = "200", description = "Ok"),
-        @ApiResponse(responseCode = "401", description = "Unauthorized"),
-        @ApiResponse(responseCode = "403", description = "Forbidden"),
-        @ApiResponse(responseCode = "400", description = "Bad request"),
-        @ApiResponse(responseCode = "408", description = "Request Timeout"),
-        @ApiResponse(responseCode = "415", description = "Unsupported media type")
-      })
+          value = {
+                  @ApiResponse(responseCode = "200", description = "Ok"),
+                  @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                  @ApiResponse(responseCode = "403", description = "Forbidden"),
+                  @ApiResponse(responseCode = "400", description = "Bad request"),
+                  @ApiResponse(responseCode = "408", description = "Request Timeout"),
+                  @ApiResponse(responseCode = "415", description = "Unsupported media type")
+          })
   @CrossOrigin(origins = "http://localhost:8000")
   @PostMapping(
-      value = "/submit",
-      consumes = {APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE})
+          value = "/submit",
+          consumes = {APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE})
   public String performSubmissionToBioSamplesAndEna(
-      @RequestBody final String submissionPayload,
-      @RequestParam(value = "webinjwt") String webinJwt)
-      throws Exception {
+          @RequestBody final String submissionPayload,
+          @RequestParam(value = "webinjwt") String webinJwt)
+          throws Exception {
     String webinToken;
 
     objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
@@ -52,9 +58,14 @@ public class BioSampleSubmissionController {
     final IsaJson isaJson = this.objectMapper.readValue(submissionPayload, IsaJson.class);
     final List<Study> studies = getStudies(isaJson);
 
-    this.bioSamplesSubmitter.createBioSamples(studies, webinToken);
+    final Map<String, String> accessionsMap = this.bioSamplesSubmitter.createBioSamples(studies, webinToken);
+    final Receipt repositoryData = new Receipt();
 
-    return this.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(isaJson);
+    repositoryData.setTargetRepository("biosamples");
+    repositoryData.setAccessions(accessionsMap.values().stream()
+            .map(value -> new Accession(value, null)).collect(Collectors.toList()));
+
+    return this.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(repositoryData);
   }
 
   public List<Study> getStudies(final IsaJson isaJson) {
