98424c36c8fae001ff52782850d9fd9111cc2d39
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-10-24 13:31:21 +0200

    linting

--- mars-cli/mars_lib/credential.py
+++ mars-cli/mars_lib/credential.py
@@ -93,12 +93,12 @@ class CredentialManager:
         :return: The password or None if not found.
         """
         pwd = keyring.get_password(self.service_name, username)
+        new_pwd = pwd
         if pwd is None:
             new_pwd = self.prompt_for_password()
             self.set_password_keyring(username, new_pwd)
-            # raise ValueError(f"Password not found for username '{username}'.")
 
-        return keyring.get_password(self.service_name, username)
+        return str(new_pwd)
 
     def delete_password_keyring(self, username: str) -> None:
         """
