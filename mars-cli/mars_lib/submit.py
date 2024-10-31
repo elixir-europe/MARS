5cb9f48b8123c59d61bd0fbacf25930c934756ba
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-10-31 13:58:03 +0100

    Add logic for BioSamples submission

--- mars-cli/mars_lib/submit.py
+++ mars-cli/mars_lib/submit.py
@@ -1,4 +1,6 @@
-from mars_lib.models.isa_json import Investigation
+import sys
+import requests
+from typing import Any
 from mars_lib.authentication import get_webin_auth_token
 from mars_lib.biosamples_external_references import (
     get_header,
@@ -7,19 +9,28 @@ from mars_lib.biosamples_external_references import (
     validate_json_against_schema,
     input_json_schema_filepath,
 )
-from mars_lib.isa_json import reduce_isa_json_for_target_repo
-from mars_lib.target_repo import TargetRepository
 from mars_lib.credential import CredentialManager
-import requests
-from typing import Any
-
+from mars_lib.isa_json import load_isa_json
+from mars_lib.models.isa_json import IsaJson, Investigation
+from mars_lib.target_repo import TargetRepository
+from mars_lib.logging import print_and_log
 
 def submission(
-    username_credentials, isa_json_file, target_repositories, investigation_is_root
+        credential_service_name,
+        username_credentials,
+        isa_json_file,
+        target_repositories,
+        investigation_is_root,
+        urls,
 ):
-    cm = CredentialManager("mars-cli")
+    # Get password from the credential manager
+    cm = CredentialManager(credential_service_name)
     pwd = cm.get_password_keyring(username_credentials)
 
+    isa_json: IsaJson = load_isa_json(isa_json_file, investigation_is_root)
+
+    # Remove the biosamples step if ENA is the repositories list
+    # Probably not the best way to address this
     if TargetRepository.ENA in target_repositories:
         target_repositories.pop(TargetRepository.BIOSAMPLES)
 
@@ -27,9 +38,15 @@ def submission(
         submit_to_ena()
     elif TargetRepository.BIOSAMPLES in target_repositories:
         # Submit to Biosamples
-        biosamples_credentials = {"username": username_credentials, "password": pwd}
+        biosamples_credentials = {
+            "username": username_credentials,
+            "password": pwd,
+        }
         submit_to_biosamples(
-            isa_json_file, biosamples_credentials, biosamples_endpoints["prod"]
+            investigation=isa_json,
+            biosamples_credentials=biosamples_credentials,
+            biosamples_url=urls["BIOSAMPLES"]["SUBMISSION"],
+            webin_token_url=urls["WEBIN"]["TOKEN"],
         )
     elif TargetRepository.METABOLIGHTS in target_repositories:
         # Submit to MetaboLights
@@ -42,15 +59,28 @@ def submission(
 
 
 def submit_to_biosamples(
-    investiagation: Investigation,
+        investigation: IsaJson,
     biosamples_credentials: dict[str, str],
-    url: str,
+        webin_token_url: str,
+        biosamples_url: str,
 ) -> requests.Response:
-
-    params = {"webinjwt": get_webin_auth_token(biosamples_credentials)}
+    params = {
+        "webinjwt": get_webin_auth_token(
+            biosamples_credentials, auth_base_url=webin_token_url
+        )
+    }
     headers = {"accept": "*/*", "Content-Type": "application/json"}
-    return requests.post(url, headers=headers, params=params, json=investiagation)
+    result = requests.post(
+        biosamples_url,
+        headers=headers,
+        params=params,
+        json=investigation.model_dump(by_alias=True, exclude_none=True),
+    )
 
+    if result.status_code != 200:
+        raise requests.HTTPError(f"Request towards BioSamples failed!\nRequest:{str(result.request)}")
+
+    return result
 
 def create_external_references(
     biosamples_credentials: dict[str, str],
