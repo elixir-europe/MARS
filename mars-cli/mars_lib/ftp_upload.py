bf052138069c244bcbd63389b2bc3b9146847568
April Shen <ashen@ebi.ac.uk>
2024-11-05 16:09:57 +0100

    format file

--- mars-cli/mars_lib/ftp_upload.py
+++ mars-cli/mars_lib/ftp_upload.py
@@ -13,12 +13,13 @@ class PatchFTP_TLS(ftplib.FTP_TLS):
     to work around bug in Python standard library: https://bugs.python.org/issue19500
     Explicit FTPS, with shared TLS session
     """
+
     def ntransfercmd(self, cmd, rest=None):
         conn, size = ftplib.FTP.ntransfercmd(self, cmd, rest)
         if self._prot_p:
-            conn = self.context.wrap_socket(conn,
-                                            server_hostname=self.host,
-                                            session=self.sock.session)  # this is the fix
+            conn = self.context.wrap_socket(
+                conn, server_hostname=self.host, session=self.sock.session
+            )  # this is the fix
         return conn, size
 
 
@@ -28,9 +29,8 @@ class FTPUploader:
         self.username = username
         self.password = password
 
-
     @retry(exceptions=ftplib.all_errors, tries=3, delay=2, backoff=1.2, jitter=(1, 3))
-    def upload(self, file_paths: List[Path], target_location: str="/") -> bool:
+    def upload(self, file_paths: List[Path], target_location: str = "/") -> bool:
         # Heuristic to set the expected timeout assuming 10Mb/s upload speed but no less than 30 sec
         # and no more than an hour
         max_file_size = max([os.path.getsize(f) for f in file_paths])
@@ -45,11 +45,15 @@ class FTPUploader:
             previous_content = ftps.nlst()
             for file_to_upload in file_paths:
                 file_name = os.path.basename(file_to_upload)
-                if file_name in previous_content and ftps.size(file_name) == os.path.getsize(file_to_upload):
-                    print_and_log(f"{file_name} already exists and has the same size on the FTP, skipping")
+                if file_name in previous_content and ftps.size(
+                    file_name
+                ) == os.path.getsize(file_to_upload):
+                    print_and_log(
+                        f"{file_name} already exists and has the same size on the FTP, skipping"
+                    )
                     continue
-                print_and_log(f'Uploading {file_name} to FTP')
-                with open(file_to_upload, 'rb') as open_file:
-                    ftps.storbinary('STOR %s' % file_name, open_file)
-                
+                print_and_log(f"Uploading {file_name} to FTP")
+                with open(file_to_upload, "rb") as open_file:
+                    ftps.storbinary("STOR %s" % file_name, open_file)
+
         return True
