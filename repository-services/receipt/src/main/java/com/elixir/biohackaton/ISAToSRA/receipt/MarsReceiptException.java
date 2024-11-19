87db6d6ea99ca48bb776cfd9da89ac17adc6eb0c
Pedram A. Keyvani <pedram.a.keyvani@gmail.com>
2024-11-19 13:05:05 +0100

    MarsReceiptException with MarsPath

--- repository-services/receipt/src/main/java/com/elixir/biohackaton/ISAToSRA/receipt/MarsReceiptException.java
+++ repository-services/receipt/src/main/java/com/elixir/biohackaton/ISAToSRA/receipt/MarsReceiptException.java
@@ -1,17 +1,53 @@
 package com.elixir.biohackaton.ISAToSRA.receipt;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.elixir.biohackaton.ISAToSRA.receipt.marsmodel.MarsError;
+import com.elixir.biohackaton.ISAToSRA.receipt.marsmodel.MarsPath;
+
 import lombok.Getter;
 
 public class MarsReceiptException extends RuntimeException {
     @Getter
-    private final String receiptErrorMessage;
+    private final MarsError error;
 
     public MarsReceiptException(final String receiptErrorMessage) {
-        this.receiptErrorMessage = receiptErrorMessage;
+        error = MarsError.builder()
+                .message(receiptErrorMessage)
+                .build();
     }
 
-    public MarsReceiptException(final String receiptErrorMessage, final Exception exception) {
+    public MarsReceiptException(final Exception exception, final String receiptErrorMessage) {
         this.addSuppressed(exception);
-        this.receiptErrorMessage = receiptErrorMessage;
+        if (exception instanceof MarsReceiptException) {
+            error = ((MarsReceiptException) exception).error;
+        } else {
+            error = MarsError.builder()
+                    .message(receiptErrorMessage)
+                    .build();
+        }
+    }
+
+    public MarsReceiptException(final String receiptErrorMessage, final MarsPath... errorPath) {
+        error = MarsError.builder()
+                .message(receiptErrorMessage)
+                .path(List.of(Optional.ofNullable(errorPath).orElse(new MarsPath[0])))
+                .build();
+    }
+
+    public MarsReceiptException(
+            final Exception exception,
+            final String receiptErrorMessage,
+            final MarsPath... errorPath) {
+        this.addSuppressed(exception);
+        if (exception instanceof MarsReceiptException) {
+            error = ((MarsReceiptException) exception).error;
+        } else {
+            error = MarsError.builder()
+                    .message(receiptErrorMessage)
+                    .path(List.of(Optional.ofNullable(errorPath).orElse(new MarsPath[0])))
+                    .build();
+        }
     }
 }
