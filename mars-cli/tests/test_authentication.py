c549803f03bfe3a9afd6f626a78d1b6995f2c365
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-11-21 14:01:33 +0100

    Update credentials file to hold multiple credentials

--- mars-cli/tests/test_authentication.py
+++ mars-cli/tests/test_authentication.py
@@ -1,24 +1,53 @@
 import pytest
-import json
 import os
-from mars_lib.authentication import get_webin_auth_token
+from mars_lib.authentication import (
+    get_webin_auth_token,
+    load_credentials,
+    get_metabolights_auth_token,
+)
+from requests.exceptions import HTTPError
+
+fake_credentials_dict = {
+    "webin": {
+        "username": "my_fake_account",
+        "password": "my_super_secret_password",
+    },
+    "metabolights_metadata": {
+        "username": "my_fake_account",
+        "password": "my_super_secret_password",
+    },
+    "metabolights_data": {
+        "username": "my_fake_account",
+        "password": "my_super_secret_password",
+    },
+}
 
 
 def test_get_webin_auth_token():
-    fake_credentials_dict = {
-        "username": "my_fake_account",
-        "password": "my_super_secret_password",
-    }
     with pytest.raises(
         ValueError,
         match="ERROR when generating token. See response's content below:\nBad credentials",
     ):
-        get_webin_auth_token(fake_credentials_dict)
+        get_webin_auth_token(fake_credentials_dict["webin"])
 
     file = "./tests/test_credentials.json"
     if os.path.exists(file) and os.path.isfile(file):
-        with open(file, "r") as f:
-            test_credentials = json.load(f)
+        test_credentials = load_credentials(file)
 
-        token = get_webin_auth_token(test_credentials)
+        token = get_webin_auth_token(test_credentials["webin"])
         assert token
+
+
+@pytest.mark.skipif(
+    not os.path.exists("./tests/test_credentials.json"),
+    reason="Credentials file not found",
+)
+def test_get_metabolights_auth_token():
+    credentials = load_credentials("./tests/test_credentials.json")
+    token = get_metabolights_auth_token(credentials["metabolights_metadata"])
+    assert token
+
+    # TODO: Currently an 'Internal Server Error' is returned when using the wrong credentials.
+    # This should be updated to return a more informative error message.
+    with pytest.raises(HTTPError):
+        get_metabolights_auth_token(fake_credentials_dict["metabolights_metadata"])
