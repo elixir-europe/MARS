32b4ac0fc28c70557bf33a61c282ce9ca3b62317
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-11-21 16:00:49 +0100

    Add validation for credential manager

--- mars-cli/tests/test_credential_manager.py
+++ mars-cli/tests/test_credential_manager.py
@@ -1,9 +1,16 @@
+import pytest
+
 from mars_lib.credential import CredentialManager
 
 
 def test_create_credentials_manager():
-    cm = CredentialManager("mars-cli")
+    cm = CredentialManager("webin")
     assert cm is not None
+
+    with pytest.raises(
+        ValueError, match="Invalid authentication provider: invalid_provider"
+    ):
+        CredentialManager("invalid_provider")
 
 
 def test_set_password_keyring():
