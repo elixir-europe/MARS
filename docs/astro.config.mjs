0b6087397114dd06bc50323c6b93382abea11915
Bert Droesbeke <bertdroesbeke@hotmail.com>
2025-09-18 11:40:37 +0200

    add github domain

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://elixir-europe.github.io',
+    base: '/MARS'
+}
+)
