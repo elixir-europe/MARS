608ed32f81656a6b1f44bca4c369973ff9ba99cb
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2025-10-03 17:42:26 +0200

    Add tailwind CSS

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://elixir-europe.github.io',
-    base: '/MARS'
+  site: 'https://elixir-europe.github.io',
+  base: '/MARS',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
